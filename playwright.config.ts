215350fc9e341e9ccf2a88df174e50f7f4d5a188
Hong Nguyen <hong.nguyen7@ntq-solution.com.vn>
2026-04-18 12:24:05 +0700

    Add initial project setup with Playwright configuration and testing framework

---
+++ playwright.config.ts
@@ -0,0 +1,101 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import os from 'node:os';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './src/tests',
+
+  timeout: 90_000,
+
+  expect: {
+    timeout: 20_000,
+  },
+
+  fullyParallel: true,
+  workers: 2,
+
+  reporter: [
+    ['list'],
+    [
+      'allure-playwright',
+      {
+        outputFolder: 'allure-results',
+        detail: true,
+        suiteTitle: true,
+        autoAttachScreenshots: true,
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
+    ],
+  ],
+
+  use: {
+    baseURL: `${process.env.BASE_URL}${process.env.PREFIX ?? ''}`,
+    permissions: ['camera', 'microphone'],
+    launchOptions: {
+      args: [
+        '--use-fake-ui-for-media-stream',
+        '--use-fake-device-for-media-stream',
+      ],
+    },
+    browserName: 'chromium',
+    headless: false,
+
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    video: 'off',
+  },
+
+  projects: [
+    {
+      name: 'setup',
+      testMatch: /auth.setup.ts/,
+    },
+    {
+      name: '01_Login',
+      testMatch: ['login.spec.ts'],
+      //dependencies: ['setup'],
+      use: {
+        ...devices['Pixel 7'],
+        //storageState: 'auth_state_user.json',
+      },
+    },
+
+    {
+      name: '02_Search',
+      testMatch: ['search.spec.ts'],
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: '03_Product Detail Page',
+      testMatch: ['product.detail.spec.ts'],
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: '04_Cart',
+      testMatch: ['cart.spec.ts'],
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: '05_Checkout',
+      testMatch: ['checkout.spec.ts'],
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+
+  outputDir: 'test-results/',
+});
