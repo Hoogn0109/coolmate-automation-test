ba8d3a93766448984bcafca7670233b5a16cc753
Hong Nguyen <hong.nguyen7@ntq-solution.com.vn>
2026-04-18 12:36:58 +0700

    Add search functionality with data, locators, and tests

---
+++ src/data/search.data.ts
@@ -0,0 +1,18 @@
+export const searchData = {
+    validSearch: "áo thun",
+    emptySearch: "",
+    invalidSearch: "!@#$%^&*()",
+    searchWithNoSpace: "aothun",
+    searchWithUpperCase: "ÁO THUN",
+    searchWithFullSpace: "   ",
+    nameProductSearch: "áo phông",
+    searchWithOneCharacter: "q",
+    searchWithEmoji: "áo thun 👏",
+    emoji: "👏"
+}
+
+export const timeDelay = {
+    shortDelay: 15000,
+    mediumDelay: 20000,
+    longDelay: 30000,
+}
